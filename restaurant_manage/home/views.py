cba97e12343737f915cdc27b54de1c749e50c1f3
nguyenngoc4855 <coasdsaza000@gmail.com>
2024-05-08 12:32:11 +0700

    Add reservation

--- restaurant_manage/home/views.py
+++ restaurant_manage/home/views.py
@@ -1,12 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from .forms import BookTableForm
+from .models import Booking
 # Create your views here.
 #view home
 
 def home(request):
-    context = {}
-    return render(request, 'home\home.html', context)
+    if request.method == 'POST':
+        form = BookTableForm(request.POST)
+        if form.is_valid():
+            # Xử lý dữ liệu
+            select_location = form.cleaned_data['select_location']
+            select_size = form.cleaned_data['select_size']
+            choice_date = form.cleaned_data['choice_date']
+            choice_time = form.cleaned_data['choice_time']
+            fname = form.cleaned_data['fname']
+            lname = form.cleaned_data['lname']
+            phone = form.cleaned_data['phone']
+            email = form.cleaned_data['email']
+            size_party = form.cleaned_data['size_party']
+            note = form.cleaned_data['note']
+            
+            # Lưu dữ liệu vào cơ sở dữ liệu
+            booking = Booking(
+                select_location=select_location,
+                select_size=select_size,
+                choice_date=choice_date,
+                choice_time=choice_time,
+                fname=fname,
+                lname=lname,
+                phone=phone,
+                email=email,
+                size_party=size_party,
+                note=note
+            )
+            booking.save()
+            
+            # Redirect hoặc render template
+            return redirect('home')
+    else:
+        form = BookTableForm()
+    
+    return render(request, 'home\home.html', {'form': form})
 
 #view event
 def event(request):
