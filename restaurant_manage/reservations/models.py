354fbd6a52f1edf0d15fc4eead5f77b655c376be
20521319 <20521319@uit.edu.vn>
2024-05-26 21:28:10 +0700

    revert

--- restaurant_manage/reservations/models.py
+++ restaurant_manage/reservations/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    select_location = models.CharField(max_length=100)
+    select_size = models.IntegerField()
+    choice_date = models.DateField()
+    choice_time = models.TimeField()
+    fname = models.CharField(max_length=100)
+    lname = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+    size_party = models.CharField(max_length=100)
+    note = models.TextField()
