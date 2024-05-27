d3b881b7f344ab52c007d3d9d7eb3b7e4325defa
20521319 <20521319@uit.edu.vn>
2024-05-27 22:46:37 +0700

    update

--- restaurant_manage/cart/views.py
+++ restaurant_manage/cart/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from deliveries.models import Order
 # Create your views here.
 def cart(request):
-    user_id = request.session.get('user_id')
-    if user_id:
+    # user_id = request.session.get('user_id')
+    # if user_id:
         #order = Order.objects.get(user=user_id)
         #print(order)
         return render(request, 'cart/cart.html')
