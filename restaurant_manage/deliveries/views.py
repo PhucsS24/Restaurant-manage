b177411e62fea1ddad8764cb16d3eddf5ae5993c
20521319 <20521319@uit.edu.vn>
2024-05-29 21:32:19 +0700

    update

--- restaurant_manage/deliveries/views.py
+++ restaurant_manage/deliveries/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from .models import *
 from django.http import HttpResponse
+from django.conf import settings
 
 # Create your views here.
 def add_to_cart(request):
+    if request.method == 'POST':
+        product_id = request.POST.get('productId')
+        quantity = int(request.POST.get('quantity'))
+        total = float(request.POST.get('total'))
 
+        cart = request.session.get(settings.CARD_SESSION_ID)
+
+        print(product_id)
+        print(cart)
     return None
 
 
